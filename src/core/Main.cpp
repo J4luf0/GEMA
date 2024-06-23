bf3f9373a376968b5e17aa7599c81f45cf0dfb3d
MartinSpanur <martinspanur@seznam.cz>
2024-06-23 22:13:05 +0200

    added test.

--- src/core/Main.cpp
+++ src/core/Main.cpp
@@ -185,6 +185,25 @@ inline void test_setItem_003(){
     assert(*tensor == *expected);
 }
 
+inline void test_transposition_001(){
+
+    cout << "test_transposition_001\n";
+
+    const vector<int> dimensionSizes{2, 3};
+    auto tensor = make_unique<Tensor<int>>(dimensionSizes);
+
+    tensor->setItems({0, 5, -1, 100, -2, -16});
+
+    Tensor<int>* result = tensor->transposition();
+    
+    const vector<int> expectedDimensionSizes{3, 2};
+    auto expected = make_unique<Tensor<int>>(dimensionSizes);
+    expected->setItems({0, -1, -2, 5, 100, -16});
+
+    assert(*result == *expected);
+    delete result;
+}
+
 inline void test_operatorAssign_001(){
 
     cout << "test_operatorAssign_001\n";
@@ -229,57 +248,11 @@ int main(){
     test_setItem_001();
     test_setItem_002();
     test_setItem_003();
+    test_transposition_001();
     test_operatorAssign_001();
 
     cout << "\nAll test done.\n";
     
-/*
-    const vector<int> dimensionSizes = {2, 3};
-    auto tensor1 = make_unique<Tensor<double>>(dimensionSizes);
-
-    tensor1->setItem(5,     {0, 0});
-    tensor1->setItem(0.55,    {1, 0});
-    tensor1->setItem(-0,     {0, 1});
-    tensor1->setItem(-2,      {1, 1});
-    tensor1->setItem(4.5,   {0, 2});
-    tensor1->setItem(7,    {1, 2});
-
-    tensor1->showTensor();
-    
-    auto tensor2 = make_unique<Tensor<double>>(dimensionSizes);
-
-    const vector<double> tensor2Items = {2, -5, 10, 5.89647, 3, 4};
-    tensor2->setItems(tensor2Items);
-
-    tensor2->showTensor();
-
-
-    cout << "Addition now.\n";
-
-    auto tensorAdd = *tensor1 + *tensor2;
-    tensorAdd->showTensor();
-
-
-    cout << '\n';
-
-    cout << "Transposition now.\n";
-    auto tensorTransposed = tensor1->transposition();
-    cout << "Tensor allocated.\n";
-    tensorTransposed->showTensor();
-
-    cout << "Is equilateral? / no\n";
-    cout << tensorTransposed->isTensorEquilateral() << '\n';
-
-    vector<int> tensorEquiDimensionSizes = {3, 3};
-    auto tensorEqui = make_unique<Tensor<double>>(tensorEquiDimensionSizes);
-    cout << "Is equilateral? / yes\n";
-    cout << tensorEqui->isTensorEquilateral() << '\n';
-
-    assert(tensorEqui->isTensorEquilateral() == 1);
-    assert(tensorTransposed->isTensorEquilateral() == 0);*/
-
-
-
     return 0;
 }
 
